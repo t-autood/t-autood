266d14feffaf89a56a62041b80b3f40e473dceb1
t-autood <129416134+t-autood@users.noreply.github.com>
2023-08-07 21:52:21 -0400

    Add files via upload

--- score_new_dataset.py
+++ score_new_dataset.py
@@ -4,28 +4,6 @@ from os.path import join, realpath, dirname
 from sklearn.pipeline import Pipeline
 from sklearn.pipeline import FeatureUnion
 
-def _set_pipeline_random_seed(pipeline, pipeline_random_seed=3046024202):
-    if isinstance(pipeline, FeatureUnion):
-        steps = pipeline.transformer_list
-    elif isinstance(pipeline, Pipeline):
-        steps = pipeline.steps
-    else:
-        steps = [('estimator', pipeline)]  # an ordinary estimator
-
-    for _, step_component in steps:
-        if isinstance(step_component, Pipeline) or isinstance(step_component, FeatureUnion):
-            _set_pipeline_random_seed(
-                                            pipeline=step_component,
-                                            pipeline_random_seed=pipeline_random_seed
-            )
-            continue
-
-        if 'random_state' in step_component.get_params():
-            step_component.set_params(random_state=pipeline_random_seed)
-
-    return pipeline
-
-
 if __name__ == "__main__":
     curr_dir = dirname(realpath(__file__))
     meta_model = joblib.load(join(curr_dir, "model_s23283215.pkl"))
@@ -43,12 +21,7 @@ if __name__ == "__main__":
     odf = pd.DataFrame()
     for pname in pipeline_names:
         pp_name = join(curr_dir, f"pipelines/{pname}.joblib")
-        pp = joblib.load(pp_name)
-        
-        pipeline = _set_pipeline_random_seed(
-            pipeline=pp,
-            pipeline_random_seed=3046024202,
-        )
+        pipeline = joblib.load(pp_name)
         pipeline.fit(X)
         y_score = pipeline.score_samples(X)
         odf[pname] = y_score.tolist()
